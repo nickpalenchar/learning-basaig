86b4080bd26bde1356f056d96a296b5e7b1ed9bb
Nick Palenchar <nickpal@nickpalenchar.com>
2021-02-20 10:49:36 -0500

    first web server

--- webadmin/main.go
+++ webadmin/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+  "log"
+  "net/http"
+  "fmt"
+)
 
 func main() {
   log.Println("This is not a web server")
+
+  http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, world!")
+  })
+
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatal("unable to start web server", err)
+  }
 }
